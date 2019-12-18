65835769570bd5f67c1c2d0b9d41a88908205d4f
MarcosAlves_19 <marcos.alves@losch.lu>
2019-12-18 00:10:49 +0000

    Setting up project

---
+++ src/components/Button.svelte
@@ -0,0 +1,5 @@
+<script>
+  export let placeholder = "";
+</script>
+
+<button>{placeholder}</button>
---
+++ src/routes/index.svelte
@@ -0,0 +1,9 @@
+<style>
+
+</style>
+
+<svelte:head>
+  <title>Home | Marcos Alves</title>
+</svelte:head>
+
+<h1>Great success!</h1>
